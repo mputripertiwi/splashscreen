421e46b1a2065281c559b3e53b56c666b4f5ed8d
Mahardika Putri P <mahardika_pertiwi_23rpl@student.smktelkom-mlg.sch.id>
2016-12-07 19:38:51 +0700

    initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/splashscreen/SplashActivity.java
@@ -0,0 +1,28 @@
+package id.sch.smktelkom_mlg.learn.splashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
+
